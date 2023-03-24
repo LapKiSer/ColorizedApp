0394169bae5a3acb9d121cf6af2249ede89a6624
Lapkovskiy Kirill <lapkovskiykirill@gmail.com>
2023-03-24 20:32:50 +0300

    Setup logics slider.

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        setupValueSliders()
+        
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        redSliderValue.text = (roundf(redSlider.value * 100) / 100).formatted()
+    }
+    @IBAction func greenSliderAction() {
+        greenSliderValue.text = (roundf(greenSlider.value * 100) / 100).formatted()
+    }
+    @IBAction func blueSliderAction() {
+        blueSliderValue.text = (roundf(blueSlider.value * 100) / 100).formatted()
+    }
+    
+    
+    private func setupValueSliders() {
+        redSliderValue.text = redSlider.value.formatted()
+        greenSliderValue.text = greenSlider.value.formatted()
+        blueSliderValue.text = blueSlider.value.formatted()
+    }
 }
 
